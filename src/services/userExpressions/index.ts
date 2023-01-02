6ae3f478d24f7c035b54207b4980577b6c4c9d3e
Micheli Ferreira <micheliferreira846@gmail.com>
2023-01-02 11:27:12 -0300

    feat: requisição para carregar expressões do backend

--- src/services/userExpressions/index.ts
+++ src/services/userExpressions/index.ts
@@ -1,6 +1,19 @@
 import { IuserExpression } from '@interfaces/IuserExpression';
 import api, { handleAxiosError } from '@services/api';
 
+export interface ExpressionInterface {
+  _id?: string;
+  expression: string;
+}
+
+export const getUsersExpressions = async () => {
+  try {
+    return api.get<{ data: ExpressionInterface[] }>(`/usersExpressions/`);
+  } catch (error) {
+    throw handleAxiosError(error);
+  }
+};
+
 export const postUserExpression = async (expression: string, token: string) => {
   try {
     return api.post<{ data: IuserExpression }>(

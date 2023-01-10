bb022a33f564e708fbe36709b1a1b71ea1a36a91
Lucas Gabriel Nunes Aquino Meireles <lucas.meireles@dbserver.com.br>
2023-01-10 15:08:20 -0300

    [Feature] DBI-181 testing page DBInclui

--- src/pages/dictionaryDbinclui/dictionaryDbinclui.test.tsx
+++ src/pages/dictionaryDbinclui/dictionaryDbinclui.test.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen, act } from '@testing-library/react';
+import { render, screen, act, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import firebase from 'firebase/compat/app';
+
 import { AxiosResponse } from 'axios';
 
 import { DictionaryDbinclui } from './index';
-import { googleProviderFunc, firebaseInitialize } from '../../firebase/config';
-import useGoogleLogin from '@hooks/useGoogleLogin';
-import { UserEntity } from '@interfaces/UserEntity';
-import { AuthContext } from '@contexts/AuthContext';
+
 import { getDbExpression } from '@services/dbExpressions';
 import { IDBExpression } from '@interfaces/IDBExpression';
 
+import { AuthContext } from '../../contexts/AuthContext';
+import { ItemList } from '@components/CardExpression/ItemList';
 const mockedNavigate = jest.fn();
 jest.mock('react-router-dom', () => {
   const useHref = jest.fn();
@@ -22,67 +21,15 @@ jest.mock('react-router-dom', () => {
   };
 });
 
-jest.mock('../../firebase/config');
-jest.mock('@hooks/useGoogleLogin');
 jest.mock('@services/dbExpressions');
-
-jest.mock('firebase/compat/app', () => ({
-  initializeApp: jest.fn(),
-  apps: ['app'],
-}));
-
-const mockGoogleProviderFunc = googleProviderFunc as jest.MockedFunction<
-  typeof googleProviderFunc
->;
-
-const mockFirebaseInitialize = firebaseInitialize as jest.MockedFunction<
-  typeof firebaseInitialize
->;
-
-const mockUseGoogleLogin = useGoogleLogin as jest.MockedFunction<
-  typeof useGoogleLogin
->;
 
 const mockGetDbExpressions = getDbExpression as jest.MockedFunction<
   typeof getDbExpression
 >;
 
-const user: UserEntity = {
-  displayName: 'Raul',
-  email: 'raul@email.com',
-  photoURL: 'www.imagem.jpg',
-  token: '123',
-  uid: '1',
-  _id: '1',
-  admin: false,
-};
-
-const setUser = jest.fn();
-
-mockGoogleProviderFunc.mockImplementation(() => {
-  return {} as firebase.auth.GoogleAuthProvider;
-});
-
-mockFirebaseInitialize.mockReturnValue({
-  app: {} as any,
-  auth: {
-    signInWithPopup: jest.fn().mockReturnValue({
-      user: {
-        getIdToken: jest.fn(),
-      },
-    }),
-  } as any,
-  googleProvider: jest.fn().mockReturnValue(true),
-});
-
-mockUseGoogleLogin.mockReturnValue({
-  signInWithGoogle: jest.fn(),
-  signOutWithGoogle: jest.fn(),
-});
-
 describe('Teste do dictionaryDbinclui', () => {
   afterEach(() => {
-    mockGetDbExpressions.mockClear();
+    jest.clearAllMocks();
   });
   it('Deve exibir o título da página "Dicionário DBInclui"', async () => {
     await act(async () => {
@@ -144,5 +91,27 @@ describe('Teste do dictionaryDbinclui', () => {
     const Expression = screen.getByText('teste');
 
     expect(Expression).toBeInTheDocument();
+  });
+  it('Se o usuario não estiver logado o clique do button favorite chama o erro ', async () => {
+    const user = null;
+    const setUser = jest.fn();
+
+    const handleFavoriteExpressionMock = jest.fn();
+    const handleDeleteExpressionMock = jest.fn();
+
+    await act(async () => {
+      render(
+        <AuthContext.Provider value={{ user, setUser }}>
+          <DictionaryDbinclui />
+        </AuthContext.Provider>,
+      );
+    });
+
+    const buttonFavorite = screen.getByRole('button', {
+      name: /favoritebutton/i,
+    });
+    const buttonDelete = screen.getByRole('button', { name: /deletebutton/i });
+
+    fireEvent.click(buttonFavorite);
   });
 });

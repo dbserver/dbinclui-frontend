aab46c620192635da40ba6f32a5f55f6c977aee9
Lucas Gabriel Nunes Aquino Meireles <lucas.meireles@dbserver.com.br>
2023-01-10 17:03:39 -0300

    :fire: [Feature] DBI-181 Finalizando test na page dictionaryDbinclui

--- src/pages/dictionaryDbinclui/dictionaryDbinclui.test.tsx
+++ src/pages/dictionaryDbinclui/dictionaryDbinclui.test.tsx
@@ -181,3 +181,47 @@ describe('Teste do dictionaryDbinclui', () => {
     expect(noLoginWarning).toBeInTheDocument();
   });
 });
+
+it('Deve renderizar um erro caso o usuario deslogado clique no botao de favoritar expressão', async () => {
+  const expressionsMock: IDBExpression[] = [
+    {
+      _id: '1',
+      expression: 'teste',
+      author: 'autor teste',
+      deleted: false,
+      favoriteOf: '123098102983019823012',
+      created_at: '2023-01-09T13:40:17.414Z',
+      updated_at: '2023-01-10T13:02:52.228Z',
+    },
+  ];
+
+  mockGetDbExpressions.mockImplementation(
+    async () =>
+      ({
+        data: { data: expressionsMock },
+      } as unknown as Promise<AxiosResponse<{ data: IDBExpression[] }>>),
+  );
+
+  const user = null;
+  const setUser = jest.fn();
+
+  await act(async () => {
+    render(
+      <AuthContext.Provider value={{ user, setUser }}>
+        <DictionaryDbinclui />
+      </AuthContext.Provider>,
+    );
+  });
+
+  const favoriteButton = screen.getByRole('button', { name: /deletebutton/i });
+
+  await act(() => {
+    fireEvent.click(favoriteButton);
+  });
+
+  const noLoginWarning = await screen.findByText(
+    'Você precisa estar logado para deletar uma Expression. 🤔',
+  );
+
+  expect(noLoginWarning).toBeInTheDocument();
+});
